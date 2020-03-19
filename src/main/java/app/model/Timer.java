eea27b8041269b76fa3d0645ff3e679e2c02ef34
rustamg070619 <rustamg070619@gmail.com>
2020-03-19 22:29:17 +0300

    commit

---
+++ src/main/java/app/model/Timer.java
@@ -0,0 +1,17 @@
+package app.model;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDateTime;
+
+@Component
+public class Timer {
+
+    private Long nanoTime = System.nanoTime();
+
+    public Long getTime() {
+        return nanoTime;
+    }
+}
